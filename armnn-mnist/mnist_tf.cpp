a56a5d33c4ceeecadc1d1de8fd139fab882cf80c
kunimune <kunimune.daisuke@google.com>
2018-07-16 20:22:14 +0900

    setting: Makefile, boost_log, command line argument

--- armnn-mnist/mnist_tf.cpp
+++ armnn-mnist/mnist_tf.cpp
@@ -35,15 +35,29 @@ armnn::OutputTensors MakeOutputTensors(const std::pair<armnn::LayerBindingId,
     return { { output.first, armnn::Tensor(output.second, outputTensorData) } };
 }
 
+#include <boost/log/trivial.hpp>
+#include "armnn/Utils.hpp"
 int main(int argc, char** argv)
 {
-    // Load a test image and its correct label
-    std::string dataDir = "data/";
-    int testImageIndex = 0;
-    std::unique_ptr<MnistImage> input = loadMnistImage(dataDir, testImageIndex);
-    if (input == nullptr)
-        return 1;
-
+	int testBatch = 10000, testCorrect = 0;
+	
+	// ロガー初期化
+	armnn::LogSeverity level = armnn::LogSeverity::Debug;
+	armnn::ConfigureLogging( true, true, level );
+	
+	// 引数表示
+	for( int ii = 0; ii < argc; ii++ ){
+		BOOST_LOG_TRIVIAL(fatal) << "argv[" << ii << "]: " << argv[ii];
+	}
+	
+	if( argc > 1 ){
+		testBatch = atoi( argv[1] );
+		if( testBatch == 0 ){
+			// 読み取りエラー
+			BOOST_LOG_TRIVIAL(warning) << "can't parse: testBatch = " << testBatch;
+		}
+	}
+	
     // Import the TensorFlow model. Note: use CreateNetworkFromBinaryFile for .pb files.
     armnnTfParser::ITfParserPtr parser = armnnTfParser::ITfParser::Create();
     armnn::INetworkPtr network = parser->CreateNetworkFromTextFile("model/simple_mnist_tf.prototxt",
@@ -55,22 +69,39 @@ int main(int argc, char** argv)
     armnnTfParser::BindingPointInfo outputBindingInfo = parser->GetNetworkOutputBindingInfo("Softmax");
 
     // Optimize the network for a specific runtime compute device, e.g. CpuAcc, GpuAcc
-    armnn::IRuntimePtr runtime = armnn::IRuntime::Create(armnn::Compute::CpuAcc);
+//	armnn::IRuntimePtr runtime = armnn::IRuntime::Create(armnn::Compute::CpuAcc);
+	armnn::IRuntimePtr runtime = armnn::IRuntime::Create(armnn::Compute::CpuRef);
     armnn::IOptimizedNetworkPtr optNet = armnn::Optimize(*network, runtime->GetDeviceSpec());
 
     // Load the optimized network onto the runtime device
     armnn::NetworkId networkIdentifier;
     runtime->LoadNetwork(networkIdentifier, std::move(optNet));
 
-    // Run a single inference on the test image
-    std::array<float, 10> output;
-    armnn::Status ret = runtime->EnqueueWorkload(networkIdentifier,
-                                                 MakeInputTensors(inputBindingInfo, &input->image[0]),
-                                                 MakeOutputTensors(outputBindingInfo, &output[0]));
-
-    // Convert 1-hot output to an integer label and print
-    int label = std::distance(output.begin(), std::max_element(output.begin(), output.end()));
-    std::cout << "Predicted: " << label << std::endl;
-    std::cout << "   Actual: " << input->label << std::endl;
+    // Load a test image and its correct label
+    std::string dataDir = "data/";
+    int testImageIndex = 0;
+    
+    for( ; testImageIndex < testBatch; testImageIndex++ )
+    {
+	    std::unique_ptr<MnistImage> input = loadMnistImage(dataDir, testImageIndex);
+	    if (input == nullptr)
+	        return 1;
+		
+    	// Run a single inference on the test image
+    	std::array<float, 10> output;
+    	armnn::Status ret = runtime->EnqueueWorkload(networkIdentifier,
+    	                                             MakeInputTensors(inputBindingInfo, &input->image[0]),
+    	                                             MakeOutputTensors(outputBindingInfo, &output[0]));
+		
+    	// Convert 1-hot output to an integer label and print
+    	int label = std::distance(output.begin(), std::max_element(output.begin(), output.end()));
+    	
+    	if( label == input->label ){ testCorrect++; }
+    }
+	std::cout << "Accuracy: " << testCorrect << " (" << testBatch << ")" << std::endl;
+    
+//	std::cout << "Predicted: " << label << std::endl;
+//	std::cout << "   Actual: " << input->label << std::endl;
+    
     return 0;
 }
